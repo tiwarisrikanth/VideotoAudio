fd712eecdf0e2cbbc6f6764c43288b75ca323689
Sriaknth Tiwari <tiwarisrikanth@gmail.com>
2020-08-09 11:10:05 +0530

    Update videotoAudio.py

--- videotoAudio.py
+++ videotoAudio.py
@@ -1,7 +1,7 @@
 import moviepy.editor as mp 
   
 # Insert Local Video File Path  
-clip = mp.VideoFileClip(r"/Users/srikanthtiwari/Desktop/video.mp4") 
+clip = mp.VideoFileClip(r"video.mp4") 
   
 # Insert Local Audio File Path 
 clip.audio.write_audiofile(r"AUDIO.mp3") 
